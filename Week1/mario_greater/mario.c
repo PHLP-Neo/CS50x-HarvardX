38cd1f6204e6283f05765b76091eba2ea37bef68
PHLP_Neo <phlp.neo@gmail.com>
2025-09-06 03:23:53 +1000

    Add "mario greater" from week 1

---
+++ Week1/mario_greater/mario.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int layer, current_line, whitespace, block;
+    do
+    {
+        layer = get_int("Height: ");
+    } while (layer <= 0);
+    // printf("%i\n",layer);
+    current_line = 1;
+    while (current_line <= layer)
+    {
+        whitespace = layer - current_line;
+        block = current_line;
+        while (whitespace > 0)
+        {
+            printf(" ");
+            whitespace = whitespace - 1;
+        }
+        while (block > 0)
+        {
+            printf("#");
+            block = block - 1;
+        }
+        whitespace = layer - current_line;
+        block = current_line;
+        printf("  ");
+        while (block > 0)
+        {
+            printf("#");
+            block = block - 1;
+        }
+        // while (whitespace > 0)
+        //{
+        //     printf(" ");
+        //     whitespace = whitespace - 1;
+        // }
+        printf("\n");
+        current_line = current_line + 1;
+    }
+    return 0;
+}
