255ef9c16239cfc1c833bb18a20a313c854deb1f
PHLP_Neo <phlp.neo@gmail.com>
2026-02-04 23:29:20 +1100

    started search function

--- cs50web/A1/wiki/wiki/encyclopedia/urls.py
+++ cs50web/A1/wiki/wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.search, name="search"),
     path("<str:entry_name>", views.show_entries, name="show_entries"),
 ]
+
--- cs50web/A1/wiki/wiki/encyclopedia/views.py
+++ cs50web/A1/wiki/wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 import markdown2
@@ -10,9 +12,9 @@ def index(request):
     })
 
 def show_entries(request,entry_name):
-    print("entry_name is:", entry_name)
+    # print("entry_name is:", entry_name)
     entry_info = util.get_entry(entry_name)
-    print(entry_info)
+    # print(entry_info)
     if entry_info:
         return render(request, "encyclopedia/entrypage.html",{
             "name": entry_name,
@@ -20,3 +22,27 @@ def show_entries(request,entry_name):
         })
     else:
         return HttpResponse(f"Hello, the page \"{entry_name}\" is not available!")
+    
+def search(request):
+    if request.method == "POST":
+        entry_list = list(map(lambda x: x.upper(),util.list_entries()))
+        print(entry_list)
+        form = request.POST['q'].upper()
+        print("form is:",type(form))
+        if form != "" and form in entry_list:
+            return HttpResponseRedirect(f"/{form}")
+        elif form == "":
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            new_form = []
+            for i in entry_list:
+                if form in i:
+                    new_form.append(i)
+            if new_form == []:
+                return HttpResponse(f"Hey, cant find it")
+            else:
+                return HttpResponse(f"{new_form}")
+
+    else:
+        return HttpResponse(f"This is the search engine! However you should not type the url manually")
+    pass
