1f2e1560c317fb705142395c83fd4f2a37c27d82
PHLP_Neo <phlp.neo@gmail.com>
2026-02-04 15:48:03 +1100

    Well, does seems like it is working, finished step 1 of Entry Page, as well as Markdown to HTML conversion

--- cs50web/A1/wiki/wiki/encyclopedia/views.py
+++ cs50web/A1/wiki/wiki/encyclopedia/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def show_entries(request,entry_name):
+    print("entry_name is:", entry_name)
+    entry_info = util.get_entry(entry_name)
+    print(entry_info)
+    if entry_info:
+        return render(request, "encyclopedia/entrypage.html",{
+            "name": entry_name,
+            "content": markdown2.markdown(entry_info)
+        })
