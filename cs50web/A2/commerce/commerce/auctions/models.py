fd5daaaedd341942eb6946e29476d7b8321f94fc
PHLP_Neo <phlp.neo@gmail.com>
2026-02-10 19:47:35 +1100

    Working on the view page, display all testing field.

---
+++ cs50web/A2/commerce/commerce/auctions/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Auction_Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    listing_image_link = models.URLField(blank=True, null=True)
+    category_image_link = models.URLField(blank=True, null=True)
+    bid_price= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} is bidding at the price of {self.bid_price}!"
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
