11101034d2d30b14c8238bc4ec43412368072c1a
PHLP_Neo <phlp.neo@gmail.com>
2026-03-03 14:30:20 +1100

    created bash script to automate test

---
+++ cs50web/week7/test0.sh
@@ -0,0 +1,6 @@
+python -c "from test0 import test_prime;test_prime(2,True)"
+python -c "from test0 import test_prime;test_prime(3,True)"
+python -c "from test0 import test_prime;test_prime(10,False)"
+python -c "from test0 import test_prime;test_prime(25,False)"
+python -c "from test0 import test_prime;test_prime(17,True)"
+python -c "from test0 import test_prime;test_prime(1,True)"
